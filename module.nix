9a25c37eec84868c81ff0ca6ff0a0bb32a05d976
enjarai <enjarai@trans.fish>
2025-08-13 14:21:46 +0200

    feck

--- module.nix
+++ module.nix
@@ -75,7 +75,7 @@
             User = cfg.user;
             Group = cfg.group;
             WorkingDirectory = cfg.package;
-            ExecStart = "${cfg.package}/bin/clickrtraining host --addr ${cfg.address} --port ${cfg.port}";
+            ExecStart = "${cfg.package}/bin/clickrtraining host --addr ${cfg.address} --port ${toString cfg.port}";
             Restart = "always";
           };
         };
